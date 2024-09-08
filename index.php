929c60af42d56e48a023377133c0e4f98b021f38
GITHUBdhvsu2022311046 <jamesdiwa18@gmail.com>
2024-09-08 17:25:59 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -6,25 +6,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>WELCOME TO ACTIVITY 1!</title>
+    <link rel="stylesheet" href="src/css/style.css">
 </head>
 <body>
-    <h1>SIGN IN</h1>
-    <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
-        <input type="email" name="email" placeholder="Enter Email" required> <br>
-        <input type="password" name="password" placeholder="Enter Password" required> <br>
-        <button type"submit" name="btn-signin">SIGN IN</button>
-    </form>
+    <div class="container">
+        <div class="form-column">
+            <h1>SIGN IN</h1>
+            <form action="dashboard/admin/authentication/admin-class.php" method="POST">
+                <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
+                <input type="email" name="email" placeholder="Enter Email" required> <br>
+                <input type="password" name="password" placeholder="Enter Password" required> <br>
+                <button type"submit" name="btn-signin">SIGN IN</button>
+            </form>
+        </div>
 
-
-    <h1>REGISTRATION</h1>
-    <form action="dashboard/admin/authentication/admin-class.php" method="POST">
-        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
-        <input type="text" name="username" placeholder="Enter Username" required> <br>
-        <input type="email" name="email" placeholder="Enter Email" required> <br>
-        <input type="password" name="password" placeholder="Enter Password" required> <br>
-        <button type="submit" name="btn-signup">SIGN UP</button>
-    </form>
+        <div class="form-column">
+            <h1>REGISTRATION</h1>
+            <form action="dashboard/admin/authentication/admin-class.php" method="POST">
+                <input type="hidden" name="csrf_token" value="<?php echo $csrf_token?>">
+                <input type="text" name="username" placeholder="Enter Username" required> <br>
+                <input type="email" name="email" placeholder="Enter Email" required> <br>
+                <input type="password" name="password" placeholder="Enter Password" required> <br>
+                <button type="submit" name="btn-signup">SIGN UP</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
